4826d152dbf4d9c942e9d56c3b93df811575b294
Swarnavo2003 <swarnavo2020@gmail.com>
2024-07-11 18:56:20 +0530

    code finised

---
+++ app/(storefront)/products/[name]/page.tsx
@@ -0,0 +1,108 @@
+import { ProductCard } from "@/app/components/storefront/ProductCard";
+import prisma from "@/app/lib/db";
+import { notFound } from "next/navigation";
+
+async function getData(productCategory: string) {
+  switch (productCategory) {
+    case "all": {
+      const data = await prisma.product.findMany({
+        select: {
+          id: true,
+          name: true,
+          description: true,
+          images: true,
+          price: true,
+        },
+        where: {
+          status: "published",
+        },
+      });
+
+      return {
+        title: "All Products",
+        data: data,
+      };
+    }
+    case "men": {
+      const data = await prisma.product.findMany({
+        select: {
+          id: true,
+          name: true,
+          description: true,
+          images: true,
+          price: true,
+        },
+        where: {
+          status: "published",
+          category: "men",
+        },
+      });
+
+      return {
+        title: "Products For Men",
+        data: data,
+      };
+    }
+    case "women": {
+      const data = await prisma.product.findMany({
+        select: {
+          id: true,
+          name: true,
+          description: true,
+          images: true,
+          price: true,
+        },
+        where: {
+          status: "published",
+          category: "women",
+        },
+      });
+
+      return {
+        title: "Products For Women",
+        data: data,
+      };
+    }
+    case "kids": {
+      const data = await prisma.product.findMany({
+        select: {
+          id: true,
+          name: true,
+          description: true,
+          images: true,
+          price: true,
+        },
+        where: {
+          status: "published",
+          category: "kids",
+        },
+      });
+
+      return {
+        title: "Products For Kids",
+        data: data,
+      };
+    }
+    default: {
+      notFound();
+    }
+  }
+}
+
+export default async function CategoriesPage({
+  params,
+}: {
+  params: { name: string };
+}) {
+  const { data, title } = await getData(params.name);
+  return (
+    <section>
+      <h1 className="font-semibold text-3xl my-5">{title}</h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {data.map((item, index) => (
+          <ProductCard item={item} key={index} />
+        ))}
+      </div>
+    </section>
+  );
+}
---
+++ app/components/storefront/Footer.tsx
@@ -0,0 +1,11 @@
+export function Footer() {
+  return (
+    <footer className="mt-16 mb-10 mx-auto px-4 md:px-6 lg:px-8">
+      <div className="border-t border-gray-900/10 sm:mt-20 lg:mt-24">
+        <p className="text-xs leading-5 text-gray-700">
+          &copy; 2024 ShoeShack. All Rights Reserved
+        </p>
+      </div>
+    </footer>
+  );
+}
